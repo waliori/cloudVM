cffca7bc389e687585f7347277c400505daef898
waliori <waliori@gmail.com>
2020-11-22 16:13:53 +0100

    adding do snapshots auto removal script  and nextcloud photo scan cron job script

---
+++ automation/index.js
@@ -0,0 +1,19 @@
+const DigitalOcean = require("do-wrapper").default;
+const instance = new DigitalOcean('0fc37c838bab5312360aa72420b30eb07fb2f94965ble7b7180a5c65aee9a659');
+var moment = require('moment');
+var schedule = require('node-schedule')
+
+schedule.schedule3ob('45 3 * * *',	() => {
+    instance.snapshots.get()
+        .then(data => {
+            for (let index = 0; index < data.snapshots.length; index++) { 
+                const element = data.snapshots[index]; 
+                var yesterday = moment().subtract(l, 'days'); 
+                yesterday.set({hour:0,minute:0,second:0,millisecond:0})
+                if(element.size_gigabytes > 8 && moment(element.created_at).isBefore(moment().set({hour:0,minute:0,second:0,millisecond:0}))){ 
+                    instance.snapshots.deleteById(element.id).then(data2 => console.log(data2)).catch(err => console.error(err))
+                }
+            }
+        })
+        .catch(err => console.error(err));
+})
---
+++ automation/nextcloud_scan_cron.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+/snap/bin/nextcloud.occ maps:scan-photos
+#/snap/bin/nextcloud.occ maps:scan-tracks
